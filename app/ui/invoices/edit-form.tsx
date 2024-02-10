41eb64cfe6e1e7af406727f92fa4bc91baf89376
guoshaodong <9409185+guoshaodong@user.noreply.gitee.com>
2024-02-10 22:46:56 +0800

    增加交互

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -7,6 +7,7 @@ import {
   CurrencyDollarIcon,
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
+import { updateInvoice } from '@/app/lib/actions';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
 
@@ -17,8 +18,10 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+
   return (
-    <form>
+    <form action={updateInvoiceWithId}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
